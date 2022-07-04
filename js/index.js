88f2408d1432425785849875ee71cc1bd677c656
Hossain Ahamed <hossainahamed6872@gmail.com>
2022-07-05 05:14:05 +0600

    sign in page bs add & container max width 100 set

--- js/index.js
+++ js/index.js
@@ -3,10 +3,3 @@
 const id = 1001;
 const link = `SignInLogIn.html?q=${id}`;
 document.getElementById('SignInLogInHyperLink').setAttribute('href', link);
-
-const signInPage = () => {
-    preloaderOn();
-}
-
-// //on it
-// preloaderOn();
