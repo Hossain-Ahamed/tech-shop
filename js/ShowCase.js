d986705b30f26f87e723afbffadc049c860647f4
Hossain Ahamed <hossainahamed6872@gmail.com>
2022-07-07 23:36:34 +0600

    cor SHOW CHECK TAHSIN

---
+++ js/ShowCase.js
@@ -0,0 +1,39 @@
+// stop hover three option in card if the screen is less than 576px
+const HoverShowOrNot = () => {
+    if (screen.width < 576) {
+      const selects =  document.getElementsByClassName('hover-content');
+      for(var i =0, il = selects.length;i<il;i++){
+        selects[i].style.display ='none';
+     }
+    
+    }
+  }
+  HoverShowOrNot();
+  
+  
+  //show case
+  const setModal = nameOfImage => {
+    document.getElementById('modal-card').textContent = '';
+    document.getElementById('modal-card').innerHTML = `
+     <div class="row g-0">
+     <div class="col-md-6 d-flex align-item-center ">
+       <img src="images/${nameOfImage}" class=" rounded-start" alt="...">
+     </div>
+     <div class="col-md-6">
+       <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam consectetur natus iste incidunt inventore dignissimos necessitatibus quis ex odit neque magni ea numquam distinctio sapiente tempore, voluptas aut expedita soluta ipsum. Iusto, odit? Adipisci labore dolorem culpa officiis nulla optio. Voluptas reprehenderit libero nostrum minima laboriosam iusto architecto adipisci doloribus veniam perspiciatis, culpa molestias fugit. Accusantium amet, debitis architecto ipsa, illum aliquam fuga ea soluta, accusamus dolore sapiente iste voluptates a tempore quam facilis nemo? Voluptatibus enim modi est aspernatur dolores porro incidunt necessitatibuto suscipit officiis ducimus nemo commodi temporibus officia minus, veritatis accusamus, eius beatae. Obcaecati earum, molestias sit corporis officia dolorem alias incidunt id quam aperiam, est error saepe adipisci consequuntur praesentium hic, dolorum optio vitae ullam iure! .</p>
+        
+       </div>
+     </div>
+   </div>
+   
+  `
+  }
+  
+  
+  // empty the modal data 
+  const modalEmpty = () => {
+    document.getElementById('modal-card').textContent = '';
+  }
+  
--- js/index.js
+++ js/index.js
@@ -4,100 +4,30 @@ const id = 1001;
 const link = `SignInLogIn.html?q=${id}`;
 document.getElementById('SignInLogInHyperLink').setAttribute('href', link);
 
-// var url = "http://gsmarena-api.herokuapp.com/device/apple_iphone_13_pro_max-11089";
-
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", url);
-
-// xhr.setRequestHeader("accept", "application/json");
-
-// xhr.onreadystatechange = function () {
-//    if (xhr.readyState === 4) {
-//       console.log(xhr.status);
-//       console.log(xhr.responseText);
-//    }};
-
-// xhr.send();
-/*
-function onGet() {
-  const url = "https://gsmarena-api.herokuapp.com/brand/apple-phones-48";
-  var headers = {}
-  
-  fetch(url, {
-      method : "GET",
-      mode: 'cors',
-      headers: headers
-  })
-  .then((response) => {
-      if (!response.ok) {
-          throw new Error(response.error)
-      }
-      return response.json();
-  })
-  .then(data => {
-      document.getElementById('messages').innerText = data.messages;
-  })
-  .catch(function(error) {
-      document.getElementById('messages').innerText = error;
-  });
-}
-*/
-// stop hover three option in card if the screen is less than 576px
-const HoverShowOrNot = () => {
-  if (screen.width < 576) {
-    const selects =  document.getElementsByClassName('hover-content');
-    for(var i =0, il = selects.length;i<il;i++){
-      selects[i].style.display ='none';
-   }
-  
-  }
-}
-HoverShowOrNot();
-
-
-//show case
-const setModal = nameOfImage => {
-  document.getElementById('modal-card').textContent = '';
-  document.getElementById('modal-card').innerHTML = `
-   <div class="row g-0">
-   <div class="col-md-6 d-flex align-item-center ">
-     <img src="images/${nameOfImage}" class=" rounded-start" alt="...">
-   </div>
-   <div class="col-md-6">
-     <div class="card-body">
-       <h5 class="card-title">Card title</h5>
-       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam consectetur natus iste incidunt inventore dignissimos necessitatibus quis ex odit neque magni ea numquam distinctio sapiente tempore, voluptas aut expedita soluta ipsum. Iusto, odit? Adipisci labore dolorem culpa officiis nulla optio. Voluptas reprehenderit libero nostrum minima laboriosam iusto architecto adipisci doloribus veniam perspiciatis, culpa molestias fugit. Accusantium amet, debitis architecto ipsa, illum aliquam fuga ea soluta, accusamus dolore sapiente iste voluptates a tempore quam facilis nemo? Voluptatibus enim modi est aspernatur dolores porro incidunt necessitatibuto suscipit officiis ducimus nemo commodi temporibus officia minus, veritatis accusamus, eius beatae. Obcaecati earum, molestias sit corporis officia dolorem alias incidunt id quam aperiam, est error saepe adipisci consequuntur praesentium hic, dolorum optio vitae ullam iure! .</p>
-      
-     </div>
-   </div>
- </div>
- 
-`
-}
-
-
-// empty the modal data 
-const modalEmpty = () => {
-  document.getElementById('modal-card').textContent = '';
-}
-
-
-// const fetchh = ()=>{
-//   const url = `https://the-mobile-store.herokuapp.com/api/phones/`;
-//   fetch(url)
-//       .then(res => res.json())
-//       .then(data => console.log(data));
-// }
-// fetchh();
-function CORSSolve() {
+function CORSSolve(url) {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if(this.readyState ==4 && this.status ==200){
-      document.getElementById('data').innerText = xhttp.responseText
+      return xhttp.responseText;
     }
   };
   //https://cors-anywhere.herokuapp.com/corsdemo
-  xhttp.open("GET","https://cors-anywhere.herokuapp.com/http://gsmarena-api.herokuapp.com/device/apple_iphone_13_pro_max-11089",true)
+  xhttp.open("GET",url,true)
   xhttp.send()
 }
-CORSSolve();
+
+
+
+//load the brand name
+
+const PhoneBrandNameContainer = document.getElementById('PhoneBrandNameContainer');
+
+const loadPhoneBrandCase = () =>{
+  PhoneBrandNameContainer.textContent = '';
+  const url= `https://cors-anywhere.herokuapp.com/http://gsmarena-api.herokuapp.com/brands`
+  const data = CORSSolve(url);
+  console.log(data);
+
+
+}
+// loadPhoneBrandCase();
