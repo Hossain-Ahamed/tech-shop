b556c82c14a13249ded20f4e0781501dd9a88691
Hossain Ahamed <hossainahamed6872@gmail.com>
2022-07-11 00:18:58 +0600

    admin log out cancel auto

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
 app.use((req, res, next) => {
-  res.clearCookie("Admin");
+  // res.clearCookie("Admin");
     if (req.cookies.user_sid && !req.session.user) {
       res.clearCookie("user_sid");
       res.clearCookie("Email")
